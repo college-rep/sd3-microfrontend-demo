17792e3a6de82f4616cf0156534e20838526ab3f
gamberfox <gamberfox@gmail.com>
2024-11-29 16:31:05 -0500

    more

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
+import {Header} from "./Header";
 
 const App = () => (
   <div className="container">
-    <div>Name: home of me </div>
+    <div>Name: hom </div>
     <div>Framework: react</div>
     <div>Language: JavaScript</div>
     <div>CSS: Empty CSS</div>
+    <Header></Header>
   </div>
 );
 const rootElement = document.getElementById("app")
---
+++ home/src/Header.jsx
@@ -0,0 +1,12 @@
+
+
+
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <div>header function from the home frontend</div>
+  )
+}
+
+// export default Header;
